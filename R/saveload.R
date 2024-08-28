ff3acbf498aac9b1ad2fe51dad79e602f397d83d
Tom Skeleton <tomax591@gmail.com>
2024-08-28 11:15:58 +0200

    split files
    moved the loading stuff into its own script

---
+++ R/saveload.R
@@ -0,0 +1,29 @@
+#' @title Try and Load a File.
+#'
+#' @description Attempt to load a file, or stop and warn if the path is incorrect.
+#' @param path The path to the file you want to load.
+#' @param error_message Message to display if an error occurs.
+try_load <- function(path, error_message) {
+  if (!file.exists(path)) {
+    message('')
+    print(error_message)
+  }
+  load(path, envir=globalenv())
+}
+
+#' @title Ask which file to load from a Path.
+#'
+#' @description Look at all of the files in a directory and be prompted to choose one of them to load.
+#' @param dir The path to the directory where your files are located.
+ask_from_path <- function(dir) {
+  last_char <- substr(dir, nchar(dir), nchar(dir))
+  if (!last_char %in% c('\\','/')) {path <- paste0(path,'\\')}
+  files <- list.files(path)
+  message('')
+  for (f in 1:length(files)) {message(f,' : ',files[f])}
+  message('')
+  input <- NULL
+  while (!is.numeric(input)) {input <- as.numeric(readline('Please use a number to choose one of the listed files: '))}
+  path_to_file <- paste0(path,files[input])
+  load(path_to_file, envir=globalenv())
+}
--- R/workspace.R
+++ R/workspace.R
@@ -59,33 +59,3 @@ backtrack_wd <- function(target, verbose=F) {
   setwd(new.wd)
   if (verbose) message('working directory is now ',getwd())
 }
-
-#' @title Try and Load a File.
-#'
-#' @description Attempt to load a file, or stop and warn if the path is incorrect.
-#' @param path The path to the file you want to load.
-#' @param error_message Message to display if an error occurs.
-try_load <- function(path, error_message) {
-  if (!file.exists(path)) {
-    message('')
-    print(error_message)
-  }
-  load(path, envir=globalenv())
-}
-
-#' @title Ask which file to load from a Path.
-#'
-#' @description Look at all of the files in a directory and be prompted to choose one of them to load.
-#' @param path The path to the directory where your files are located.
-ask_from_path <- function(path) {
-  files <- list.files(path)
-  message('')
-  for (f in 1:length(files)) {
-    message(f,' : ',files[f])
-  }
-  message('')
-  input <- NULL
-  while (!is.numeric(input)) {input <- as.numeric(readline('Please use a number to choose one of the listed files: '))}
-  path_to_file <- paste0(path,'\\',files[input])
-  load(path_to_file, envir=globalenv())
-}
