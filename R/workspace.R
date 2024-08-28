82bb64d1802fc9a9ce096511e2c9c61b98f782cc
Tom Skeleton <tomax591@gmail.com>
2024-08-28 10:55:48 +0200

    ask_from_path
    new function allowing a user to input a path and be prompted which file to open

--- R/workspace.R
+++ R/workspace.R
@@ -72,3 +72,20 @@ try_load <- function(path, error_message) {
   }
   load(path, envir=globalenv())
 }
+
+#' @title Ask which file to load from a Path.
+#'
+#' @description Look at all of the files in a directory and be prompted to choose one of them to load.
+#' @param path The path to the directory where your files are located.
+ask_from_path <- function(path) {
+  files <- list.files(path)
+  message('')
+  for (f in 1:length(files)) {
+    message(f,' : ',files[f])
+  }
+  message('')
+  input <- NULL
+  while (!is.numeric(input)) {input <- as.numeric(readline('Please use a number to choose one of the listed files: '))}
+  path_to_file <- paste0(path,'\\',files[input])
+  load(path_to_file, envir=globalenv())
+}
