6d6889d404c85a9c2ba5157fa75dc967e61db4d5
Tom Skeleton <tomax591@gmail.com>
2024-08-28 17:23:08 +0200

    1.0
    never done a version before, probably wont do another one beucase ill forget

--- R/packages.R
+++ R/packages.R
@@ -1,7 +1,7 @@
 
 #' @title Git It!
 #'
-#' @description Try to load a package you got from github, or, install it from github if you do not have it yet. If you do not have devtools the function will first install that.
+#' @description Try to load a package you got from github, or, install it from github if you do not have it yet. If you do not have devtools the function will first install that. After doing all that, spin up the library in the current environment.
 #' @param repo The name of the Github repository to load from, as if using install_github. If already installed will use the package portion to load the library.
 #' @param quiet Stop the function from printing messages to your console.
 git_it <- function(repo, quiet=F) {
@@ -9,26 +9,32 @@ git_it <- function(repo, quiet=F) {
   package = strsplit(repo,'/')[[1]][2]
   devtools.installed <- 'devtools' %in% rownames(installed.packages())
   package.installed <- package %in% rownames(installed.packages())
+  # if devtools is not installed, get it
   if (!devtools.installed) {
     if(!quiet){message('\nYou do not have devtools installed, so I am doing that first.')}
     install.packages("devtools")
   }
+  # if package has never been installed
   if (!package.installed) {
     if(!quiet){message('\n',package,' was never installed, so I am getting the latest version from github.\n')}
     devtools::install_github(repo)
   }
+  # if package has been installed
   else {
     gh.url <- sprintf("https://raw.githubusercontent.com/%s/master/DESCRIPTION",repo)
     url.con <- url(gh.url)
     github.version <- as.character(read.dcf(url.con, fields="Version"))
     close(url.con)
     current.version <- installed.packages()[package,]['Version']
+    # if package is out of date
     if (current.version != github.version) {
       if (!quiet) {message('\n',package,' is installed but out of date (',current.version,'), getting the new version (',github.version,').\n')}
       devtools::install_github(repo)
     }
-    else {if(!quiet){message('\n',package,' is installed and up to date (',current.version,').\n')}}
   }
+  # package must now be installed and up to date
+  if(!quiet){message('I have gitten you ',package,' version ',current.version,'.\n')}
+  library(package, character.only=T)
 }
 
 #' @title CRAN It!
@@ -37,4 +43,5 @@ git_it <- function(repo, quiet=F) {
 #' @param package The name of the package to try and load.
 cran_it <- function(package) {
   if(!package %in% rownames(installed.packages())){install.packages(package)}
+  library(package, character.only=T)
 }
