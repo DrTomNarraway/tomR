73b2c24f58e60e05cebfecf1490f1b3133b76d35
Tom Skeleton <tomax591@gmail.com>
2024-09-17 15:48:05 +0200

    load_or_do
    new function to load a file or do something

--- R/saveload.R
+++ R/saveload.R
@@ -33,3 +33,20 @@ ask_from_path <- function(dir) {
   }
   load(path_to_file, envir=globalenv())
 }
+
+#' @title Load a File or do Something.
+#'
+#' @description Attempt to load file at this path, or save to this path if something was done.
+#' @param load The path to the file you want to load, or save to if nothing is loaded.
+#' @param do Function to perform if no file can be loaded.
+#' @param quiet Suppress print and message commands. Default to false.
+load_or_do <- function(path, do, quiet=F) {
+  file_exists <- file.exists(path)
+  if (file_exists) {
+    if(!quiet){message('Loading...')}
+    load(path)
+  } else {
+    if(!quiet){message('Doing...')}
+    do()
+  }
+}
