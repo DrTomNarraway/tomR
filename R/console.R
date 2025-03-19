51322208d01ac38fca148e8b262508596f360f33
DrTomNarraway <tomax591.de@gmail.com>
2025-03-19 12:06:48 +0100

    new cat stuff

--- R/console.R
+++ R/console.R
@@ -65,3 +65,37 @@ pride <- function(..., sep=' ', lb=TRUE){
   if (lb) {msg <- paste(msg,'\n')}
   cat(msg)
 }
+
+#' @title Progress (of this loop).
+#'
+#' @description cat print the current progress of the loop as a percentage of x over y.
+#' Always uses a \\r call to clear the line and replace the current percentage.
+#' @param x Numeric variable indicating the current state of the loop
+#' @param y Numeric variable indicating the end of the loop.
+#' @param dps Number of decimal places to round the percentage to.
+#' @param colour The colour to print the percentage in. Must be available in \code{\link{ansi_colours}}.
+progress <- function(x, y, dps=0, colour='white') {
+  cat('\r',tomR::ansi_colours[colour],paste0(round((x/y)*100, dps),'%'),'\033[0m        ')
+}
+
+#' @title Spinner
+#'
+#' @encoding UTF-8
+#' @description cat print a symbol to show the script is working. Always uses a \\r call to clear the line and replace the current percentage.
+#' @param i Numeric variable indicating the current state of the loop.
+#' @param set Symbol set to use for spinner. The following sets are currently available:
+#' \itemize{
+#'  \item  circle:   `r "\U25D0"`, `r "\U25D3"`, `r "\U25D1"`, `r "\U25D2"`
+#'  \item  corner:   `r "\U2599"`, `r "\U259B"`, `r "\U259C"`, `r "\U259F"`
+#'  \item  quadrant: `r "\U2596"`, `r "\U2598"`, `r "\U259D"`, `r "\U2597"`
+#' }
+#' @param colour The colour to print the spinner in. Must be available in \code{\link{ansi_colours}}.
+#' @md
+spinner <- function(i, set='circle', colour='white') {
+  symbols = list(
+    'circle' = c("\U25D0","\U25D3","\U25D1","\U25D2"),
+    'corner' = c("\U2599","\U259B","\U259C","\U259F"),
+    'quadrant' = c("\U2596","\U259D","\U2598","\U2597")
+  )
+  cat('\r',tomR::ansi_colours[colour],symbols[[set]][(i%%length(symbols[[set]]))+1],'\033[0m        ')
+}
