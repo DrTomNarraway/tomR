286ede55adafe4524ea44ea407307cf30f0665d7
Tom Skeleton <tomax591@gmail.com>
2025-03-20 14:48:19 +0100

    nested progress

--- R/console.R
+++ R/console.R
@@ -68,9 +68,9 @@ pride <- function(..., sep=' ', lb=TRUE){
 
 #' @title Progress (of this loop).
 #'
-#' @description cat print the current progress of the loop as a percentage of x over y.
+#' @description cat the current progress of the loop as a percentage of x over y.
 #' Always uses a \\r call to clear the line and replace the current percentage.
-#' @param x Numeric variable indicating the current state of the loop
+#' @param x Numeric variable indicating the current state of the loop.
 #' @param y Numeric variable indicating the end of the loop.
 #' @param dps Number of decimal places to round the percentage to.
 #' @param colour The colour to print the percentage in. Must be available in \code{\link{ansi_colours}}.
@@ -78,10 +78,23 @@ progress <- function(x, y, dps=0, colour='white') {
   cat('\r',tomR::ansi_colours[colour],paste0(round((x/y)*100, dps),'%'),'\033[0m        ')
 }
 
+#' @title Nested Progress (of some loops).
+#'
+#' @description cat the current progress of a set of nested loops. Always uses a \\r call to clear the line and replace the current percentage.
+#' @param ... Any number of pairs of the format c(current, max). Each listed pair will become one percentage display in the console.
+#' @param dps Number of decimal places to round the percentage to.
+nested_progress <- function (..., dps=0) {
+  args = list(...)
+  ps = rep(NA, length(args))
+  for (i in 1:length(args))
+    ps[i] = paste0(tomR::ansi_colours[i+1],stringr::str_pad(paste(round((args[[i]][1]/args[[i]][2])*100, dps)),3,pad="0"),'%\033[0m')
+  cat("\r",ps,'        ')
+}
+
 #' @title Spinner
 #'
 #' @encoding UTF-8
-#' @description cat print a symbol to show the script is working. Always uses a \\r call to clear the line and replace the current percentage.
+#' @description cat a symbol to show the script is working. Always uses a \\r call to clear the line and replace the current percentage.
 #' @param i Numeric variable indicating the current state of the loop.
 #' @param set Symbol set to use for spinner. The following sets are currently available:
 #' \itemize{
