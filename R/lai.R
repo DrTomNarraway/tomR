5d922d9b239b633a79a45adb2f33d7db93d3908b
DrTomNarraway <tomax591.de@gmail.com>
2025-04-08 10:34:11 +0200

    lai fix
    fixed error in lai formula and elaborated on description

--- R/lai.R
+++ R/lai.R
@@ -2,6 +2,13 @@
 #' @title Log A Index
 #'
 #' @description Estimate Log A Index (Dennis & Evans, 1996) for the given block.
+#' LAI is a measure of ability regardless of the participants chosen speed-accuracy
+#' threshold. LAI can be formally expressed as,
+#' \deqn{LAI = \frac{-1}{RT - s} \cdot \log{\frac{A - logit(p)}{A}} }
+#' where A is asymptotic accuracy, that is the greatest value logit(p) can take,
+#' s is the minimum time necessary to respond above chance (i.e. non-decision time),
+#' p is the probability of a correct response, and logit(p) is the log odds in favor
+#' of a correct response.
 #' @param rt Numeric vector of response times.
 #' @param pc Numeric vector of proportion correct scores. The average PC score for
 #' this block can be taken as p, the probabilty of a correct response.
@@ -18,6 +25,6 @@ lai <- function(rt, pc, s=0.305, A=5.294, max_p=0.995) {
   if (length(rt) != length(pc)) stop('Passed vectors are not the same length.')
   p = min(mean(pc), max_p)
   logit_p = log(p / (1-p))
-  lai = (-1 * (rt / s)) * log((A - logit_p) / A)
+  lai = (-1 / (rt / s)) * log((A - logit_p) / A)
   return(lai)
 }
